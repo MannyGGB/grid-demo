140109ae0b5401a9278ac44008e3e45ee9399ad4
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-23 14:14:20 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Grid from "./components/Grid";
+
+function App() {
+  return (
+    <div>
+      <h1>A grid!</h1>
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,4 @@
+function Profile() {
+  return <p>Manny is best.</p>;
+}
+export default Profile;
